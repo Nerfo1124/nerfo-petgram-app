6584bbf0677bad44977491d8931c7405b899928d
Nerfo_1124 <fernando.desarrollo1124@gmail.com>
2022-07-25 22:00:40 -0500

    Class 13, advancing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
 
-import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './components/Logo'
+import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+
+import { GlobalStyle } from './styles/GlobalStyles'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 )
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
+import data from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3, 4, 5, 6].map(category => <Item key={category}><Category /></Item>)
+        data.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )

02559f2271023fd33e6133d6c5c0fc3d669f19cb
Nerfo_1124 <fernando.desarrollo1124@gmail.com>
2022-07-30 16:52:27 -0500

    Update - Class 18

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import data from '../../../api/db.json'
+
+function useCategoriesData() {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  // Recuperar Categorias
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('https://nerfo-petgram-server.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+    setLoading(false)
+  }, [])
+
+  return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  // Mostrar categorias en cuadro de tamaño menor
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        data.categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  if (loading) {
+    return 'Cargando...'
+  }
+
+  return (
+    // eslint-disable-next-line react/jsx-fragments
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles.js'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles.js'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    import('intersection-observer')
+      .then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            console.log('Si')
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(element.current)
+      })
+  }, [element])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
   )
 }
